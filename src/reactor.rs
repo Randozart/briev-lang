f618654e8008b7250cb12dd69ac4a3373d8ca360
Randy Smits-Schreuder Goedheijt <randozart@gmail.com>
2026-09-23 08:21:28 +0200

    fix(metaprogramming): add variadic_param to reactor test Definition literal
    Field propagation for Definition.variadic_param (missed one test site in
    the Phase 1 commit).

--- src/reactor.rs
+++ src/reactor.rs
@@ -524,6 +524,7 @@ mod tests {
     fn test_build_from_program_skips_non_reactive() {
         let prog = simple_program(vec![
             TopLevel::Definition(Definition {
+                variadic_param: None,
                 name: "foo".into(), type_params: vec![], parameters: vec![], outputs: vec![],
                 output_type: None,
                 contract: Contract::new(Expr::Bool(true), Expr::Bool(true)),
